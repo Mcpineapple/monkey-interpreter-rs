278f6d696e1f2f8cb98c35316d07c147385de22a
Tristan Bincaz <tristan.bincaz@gmail.com>
2026-02-09 23:31:12 +0100

    moved tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -48,3 +48,61 @@ impl<'a> Lexer<'a> {
         tok
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_NextToken_One() {
+        let input = "=+(){},;";
+
+        let mut tests: Vec<Token> = Vec::new();
+        tests.push(Token::Assign);
+        tests.push(Token::Plus);
+        tests.push(Token::Lparen);
+        tests.push(Token::Rparen);
+        tests.push(Token::Lbrace);
+        tests.push(Token::Rbrace);
+        tests.push(Token::Comma);
+        tests.push(Token::Semicolon);
+        tests.push(Token::Eof);
+
+        let mut l = Lexer::new(input);
+
+        for t in tests {
+            let tok = l.nextToken();
+
+            assert_eq!(tok, t);
+        }
+    }
+    #[test]
+    fn test_NextToken_Two() {
+        let input = "let five = 5;
+let 10 = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten)";
+
+        let mut tests: Vec<Token> = Vec::new();
+        tests.push(Token::Assign);
+        tests.push(Token::Plus);
+        tests.push(Token::Lparen);
+        tests.push(Token::Rparen);
+        tests.push(Token::Lbrace);
+        tests.push(Token::Rbrace);
+        tests.push(Token::Comma);
+        tests.push(Token::Semicolon);
+        tests.push(Token::Eof);
+
+        let mut l = Lexer::new(input);
+
+        for t in tests {
+            let tok = l.nextToken();
+
+            assert_eq!(tok, t);
+        }
+    }
+}
