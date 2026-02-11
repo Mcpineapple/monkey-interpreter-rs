67aef21bbe60d3f8a249a9ad210b06c7a9492752
Tristan Bincaz <tristan.bincaz@gmail.com>
2026-02-11 22:20:54 +0100

    impl tostring for token

--- src/token.rs
+++ src/token.rs
@@ -51,3 +51,48 @@ pub fn stringToToken(string: String) -> Token {
         other => Token::Ident(string),
     }
 }
+
+impl ToString for Token {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Illegal => "Illegal token".to_string(),
+            Self::Eof => "End of File".to_string(),
+
+            // Identifiers + literals
+            Self::Ident(cont) => format!("Identifier : {}", cont),
+            Self::Int(num) => format!("Int : {}", num),
+
+            // Operators
+            Self::Assign => "=".to_string(),
+            Self::Plus => "+".to_string(),
+            Self::Minus => "-".to_string(),
+            Self::Bang => "!".to_string(),
+            Self::Asterisk => "*".to_string(),
+            Self::Slash => "/".to_string(),
+
+            Self::Lt => "<".to_string(),
+            Self::Gt => ">".to_string(),
+
+            Self::Eq => "==".to_string(),
+            Self::Neq => "!=".to_string(),
+
+            // Delimiters
+            Self::Comma => ",".to_string(),
+            Self::Semicolon => ";".to_string(),
+            Self::Lparen => "(".to_string(),
+            Self::Rparen => ")".to_string(),
+            Self::Lbrace => "{".to_string(),
+            Self::Rbrace => "}".to_string(),
+
+            // Keywords
+            Self::Function => "Function".to_string(),
+            Self::Let => "Let".to_string(),
+            Self::True => "True".to_string(),
+            Self::False => "False".to_string(),
+            Self::If => "If".to_string(),
+            Self::Else => "Else".to_string(),
+            Self::Return => "Ret".to_string(),
+            _ => "Error".to_string(),
+        }
+    }
+}
