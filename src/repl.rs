a6cf7712a5bb496f20548f26ddf49939e7957130
Tristan Bincaz <tristan.bincaz@gmail.com>
2026-02-11 19:05:35 +0100

    small io repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,1 +1,20 @@
+use crate::lexer;
+use crate::token;
+use std::io;
+use std::io::Write;
 
+const PROMPT: &str = ">> ";
+
+fn start() {
+    loop {
+        println!("{}", PROMPT);
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input).unwrap();
+        let mut l = lexer::Lexer::new(&input);
+
+        while l.ch != '\x00' {
+            println!("{:?}\n", l.nextToken());
+        }
+    }
+}
