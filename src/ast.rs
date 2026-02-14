ad10fa5335746f5a6b48281bbc7d2e461cab7ced
Tristan Bincaz <tristan.bincaz@gmail.com>
2026-02-14 17:16:57 +0100

    switched ast to enums and adjusted tests

--- src/ast.rs
+++ src/ast.rs
@@ -1,59 +1,16 @@
+use crate::token::Token;
 use std::fmt::format;
 
-use crate::token::Token;
-trait Node {
-    fn tokenLiteral(&self) -> String;
+#[derive(PartialEq, Debug)]
+pub enum Statement {
+    LetStatement(Token, Token, Expression),
 }
 
-trait Statement: Node {
-    fn statementNode(&self);
+#[derive(PartialEq, Debug)]
+pub enum Expression {
+    Identifier(Token),
 }
 
-trait Expression: Node {
-    fn expressionNode(&self);
-}
-
-struct Program {
-    statements: Vec<Box<dyn Statement>>,
-}
-
-impl Node for Program {
-    fn tokenLiteral(&self) -> String {
-        if self.statements.len() > 0 {
-            self.statements[0].tokenLiteral()
-        } else {
-            "".to_string()
-        }
-    }
-}
-
-struct LetStatement {
-    token: Token, // let
-    name: Identifier,
-    value: Box<dyn Expression>,
-}
-
-impl Node for LetStatement {
-    fn tokenLiteral(&self) -> String {
-        self.token.to_string()
-    }
-}
-
-impl Statement for LetStatement {
-    fn statementNode(&self) {}
-}
-
-struct Identifier {
-    token: Token,
-    value: String,
-}
-
-impl Node for Identifier {
-    fn tokenLiteral(&self) -> String {
-        self.token.to_string()
-    }
-}
-
-impl Expression for Identifier {
-    fn expressionNode(&self) {}
+pub struct Program {
+    pub statements: Vec<Statement>,
 }
--- src/parser.rs
+++ src/parser.rs
@@ -24,4 +24,65 @@ impl Parser {
         self.curToken = self.peekToken.clone();
         self.peekToken = self.l.nextToken();
     }
+
+    fn parseProgram(&mut self) -> ast::Program {
+        let mut prog = ast::Program {
+            statements: Vec::new(),
+        };
+
+        while self.curToken != token::Token::Eof {
+            prog.statements.push(self.parseStatement().unwrap());
+        }
+
+        prog
+    }
+
+    fn parseStatement(&self) -> Option<ast::Statement> {
+        todo!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ast::Statement;
+
+    use super::*;
+    #[test]
+    fn test_LetStatements() {
+        let input = "
+let x = 5;
+let y = 10;
+let foobar = 838383;
+";
+        let l = lexer::Lexer::new(input);
+        let mut p = Parser::new(l);
+
+        let program = p.parseProgram();
+
+        assert_ne!(program.statements.len(), 3);
+
+        let tests = vec!["x", "y", "foobar"];
+
+        for i in 0..3 {
+            assert!(testLetStatement(&program.statements[i], tests[i]))
+        }
+    }
+
+    fn testLetStatement(s: &ast::Statement, name: &str) -> bool {
+        if let ast::Statement::LetStatement(t, n, v) = s {
+            if *t != token::Token::Let {
+                println!("token is not let");
+                return false;
+            }
+
+            if let n = token::Token::Ident(name.to_string()) {
+                true
+            } else {
+                false
+            }
+        } else {
+            println!("statement is not let");
+            false
+        }
+    }
 }
